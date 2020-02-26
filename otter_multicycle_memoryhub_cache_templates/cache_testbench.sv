c24bb51386c5c056e5279e773844175b45dbd5b1
kigwe <43759582+kigwe@users.noreply.github.com>
2020-02-25 20:50:10 -0800

    ye

--- otter_multicycle_memoryhub_cache_templates/cache_testbench.sv
+++ otter_multicycle_memoryhub_cache_templates/cache_testbench.sv
@@ -5,7 +5,7 @@
 // Credits: adapted from code by Joseph Callenes-Sloan
 // 
 // Create Date: 02/07/2020 02:06:59 PM
-// Updated Date: 02/14/2020 05:00:00 AM
+// Updated Date: 02/22/2020 09:00:00 AM
 // Design Name: 
 // Module Name: cache_testbench
 // Project Name: 
@@ -22,12 +22,14 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+parameter DISPLAY_TIME = 0;  // 1 to include timestamps in log, 0 to omit them
+        
 import memory_bus_sizes::*;
 
 module cache_testbench();
 
     parameter CACHE_LINES = 256;  // power of 2, customize to the specific cache implementation 
-    
+
     // 100MHz clock
     logic clk = 0;
     initial begin
@@ -57,7 +59,7 @@ module cache_testbench();
     vlm_protocol_validator #(.DISPLAY_NAME("dcache<->ram")) vpv2 (.clk(clk), .vlm(s_dcache_to_ram.vlm_validator), .err());
     
     initial begin
-        $timeformat(-9, 3, "ns", 10);
+        $timeformat(-9, 0, "ns", 7);
         // for debugging, use Z when the cache shouldn't depend on the signals
         s_dcache.waddr = 'Z;
         s_dcache.be = 'Z;
@@ -67,30 +69,90 @@ module cache_testbench();
         s_dcache.din = 'Z;
         ##50;
 
-        $strobe("%t: ==== Read, miss, clean line (allocate) ========================================================", $time);
-        read_dcache('h01234, 'h02, 'h0);
-        ##50;
+        // initialize ram to specific random values to make the output more deterministic and diffable
+        ram.bram.r_ram['h0000000] = 'h4b7dad7111aa23301944fd6db2055e5f;
+        ram.bram.r_ram['h0123401] = 'h8832e97688f820c98b3373b29827043e;
+        ram.bram.r_ram['h0123402] = 'hfeb1a64ac99c9e59efd2d505e68aa20b;
+        ram.bram.r_ram['h0432101] = 'he190eac386e48fc0d294bf22fb0648b7;
+        ram.bram.r_ram['h0432102] = 'h3d6ddcdfe042910dd0812e19c2679427;
+        ram.bram.r_ram['h0567801] = 'hdabe3c23a2eb5e42ff4158af37a84ee7;
+        ram.bram.r_ram['h0567802] = 'he57d81631efec7a2e72720cd2315ed3c;
+        ram.bram.r_ram['h0876501] = 'h9e10c863c03bbd86c1ea25e4e668797c;
+        ram.bram.r_ram['h0876502] = 'h2793f810f8cf0486e3271bc9c12b7e93;
+        ram.bram.r_ram['h0123405] = 'h1f68e1863f49e9a98fc0b571d100b910;
+        ram.bram.r_ram['h0123406] = 'h4ac678e5e3eb7dacf8b426ec3b729619;
+        ram.bram.r_ram['h0432105] = 'h1641a2304ddf4124a4a370881c56a4b1;
+        ram.bram.r_ram['h0432106] = 'h92954f88cf492658c2b04d7d027f9dd1;
+        ram.bram.r_ram['h0567805] = 'h8c2ee818a687c3efcfdb5ba3c3b43e7c;
+        ram.bram.r_ram['h0567806] = 'h7b07db7a7d16e0c63765927a1b67dc43;
+        ram.bram.r_ram['h0876505] = 'h91e2e5f0cb59beded29e7b8b4604a9a8;
+        ram.bram.r_ram['h0876506] = 'he5396c6406dec455979957fa7044fe8d;
 
-        $strobe("%t: ==== Read, hit, clean line ====================================================================", $time);
-        read_dcache('h01234, 'h02, 'h2);
-        ##50;
-        
-        $strobe("%t: ==== Write, hit, clean line (cache line is dirty afterwards) ==================================", $time);
-        write_dcache('h01234, 'h02, 'h2, 'b1111, 'hdeadbeef);
-        ##50;
-        
-        $strobe("%t: ==== Write, conflict miss, (write back then allocate, cache line dirty) =======================", $time);
-        write_dcache('h04321, 'h02, 'h2, 'b1111, 'hcafebeef);
-        ##50;
 
-        $strobe("%t: ==== Read, hit, dirty cache line ==============================================================", $time);
-        read_dcache('h04321, 'h02, 'h0);
+        $strobe("%s==== (checking invalid tag match) Read, cold-start miss (allocate, block now valid and clean) ==================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+        read_dcache('h00000, 'h00, 'h0);
         ##50;
         
-        $strobe("%t: ==== Read, conflict miss, dirty cache line (write back then allocate, cache line is clean) ====", $time);
-        read_dcache('h05678, 'h02, 'h1);
-        ##50;
-        
+        // run tests first with delays, then without
+        for (int i = 0; i < 2; i++) begin
+
+            if (i == 0) begin
+                $strobe("%s================ Testing various cases, with delays between ====================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            end else begin
+                $strobe("%s================ Testing various cases, with no delays between, and partial-word writes ========================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            end
+    
+            $strobe("%s==== Read, cold-start miss (allocate, block now valid and clean) ===============================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h01234, 'h01 + i * 4, 'h0);
+            if (i == 0) ##50;
+            
+            $strobe("%s==== Write, cold-start miss (allocate then update, block now valid and dirty) ==================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            write_dcache('h01234, 'h02 + i * 4, 'h1, i == 0 ? 'b1111 : 'b1100, 'haabbccdd);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Read, hit, clean block (block still clean) ================================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h01234, 'h01 + i * 4, 'h2);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Read, hit, dirty block (block still dirty) ================================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h01234, 'h02 + i * 4, 'h3);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Read, conflict miss, clean block (allocate, block still clean) ============================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h04321, 'h01 + i * 4, 'h0);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Read, conflict miss, dirty block (write back then allocate, block now clean) ==============================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h04321, 'h02 + i * 4, 'h1);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Write, hit, clean block (update, block now dirty) =========================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            write_dcache('h04321, 'h01 + i * 4, 'h2, i == 0 ? 'b1111 : 'b0011, 'hbbccddee);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Write, hit, dirty block (update, block still dirty) =======================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            write_dcache('h04321, 'h01 + i * 4, 'h3, i == 0 ? 'b1111 : 'b1000, 'hccddeeff);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Write, conflict miss, clean block (allocate then update, block now dirty) =================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            write_dcache('h05678, 'h02 + i * 4, 'h0, i == 0 ? 'b1111 : 'b0100, 'hddeeffaa);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== Write, conflict miss, dirty block (write back then allocate then update, block still dirty) ===============", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            write_dcache('h05678, 'h01 + i * 4, 'h1, i == 0 ? 'b1111 : 'b0010, 'heeffaabb);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== (verifying state) Read, conflict miss, dirty block (write back then allocate, block now clean) ============", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h08765, 'h01 + i * 4, 'h2);
+            if (i == 0) ##50;
+    
+            $strobe("%s==== (verifying state) Read, conflict miss, dirty block (write back then allocate, block now clean) ============", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
+            read_dcache('h08765, 'h02 + i * 4, 'h3);
+            if (i == 0) ##50;
+
+        end
+
+        $strobe("%s================ DONE ==========================================================================================", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
         ##50;
         $finish();
     end
@@ -103,9 +165,11 @@ module cache_testbench();
         s_dcache.we <= 0;
         s_dcache.waddr <= {tag, index, block_offset};
         s_dcache.en <= 1;
-        $strobe("%t: [CPU] read addr=%x", $time, {s_dcache.waddr, {WORD_ADDR_LSB{1'b0}}});
+        $strobe("%s[CPU] read waddr=%x (addr=%x..%x)", DISPLAY_TIME ? $sformatf("%t: ", $time) : "",
+                s_dcache.waddr, {s_dcache.waddr, {WORD_ADDR_LSB{1'b1}}}, {s_dcache.waddr, {WORD_ADDR_LSB{1'b0}}});
         @(posedge clk iff !s_dcache.hold);
-        $strobe("%t: [CPU] got data=%x", $time, s_dcache.dout);
+        #0;  // in case both memory and cpu operations complete at the same time, try to report the cpu one last
+        $strobe("%s[CPU] got data=%x", DISPLAY_TIME ? $sformatf("%t: ", $time) : "", s_dcache.dout);
         s_dcache.we <= 'Z;
         s_dcache.waddr <= 'Z;
         s_dcache.en <= 0;
@@ -118,20 +182,43 @@ module cache_testbench();
         input [WORD_SIZE-1:0] byte_enable,
         input [WORD_WIDTH-1:0] data
         );
+        string masked_data;
+        masked_data = byte_mask(byte_enable, data);
         s_dcache.we <= 1;
         s_dcache.waddr <= {tag, index, block_offset};
         s_dcache.be <= byte_enable;
         s_dcache.din <= data;
         s_dcache.en <= 1;
-        $strobe("%t: [CPU] write addr=%x with data=%x", $time, {s_dcache.waddr, {WORD_ADDR_LSB{1'b0}}}, s_dcache.din);
+        $strobe("%s[CPU] write waddr=%x (addr=%x..%x) with data=%s", DISPLAY_TIME ? $sformatf("%t: ", $time) : "",
+                s_dcache.waddr, {s_dcache.waddr, {WORD_ADDR_LSB{1'b1}}}, {s_dcache.waddr, {WORD_ADDR_LSB{1'b0}}},
+                masked_data);
         @(posedge clk iff !s_dcache.hold);
-        $strobe("%t: [CPU] write accepted", $time);
+        #0;  // in case both memory and cpu operations complete at the same time, try to report the cpu one last
+        $strobe("%s[CPU] write accepted", DISPLAY_TIME ? $sformatf("%t: ", $time) : "");
         s_dcache.we <= 'Z;
         s_dcache.waddr <= 'Z;
         s_dcache.be <= 'Z;
         s_dcache.din <= 'Z;
         s_dcache.en <= 0;
     endtask
+
+    function string byte_mask(input logic [WORD_SIZE-1:0] byte_enable, input logic [WORD_WIDTH-1:0] data); begin
+        string s;
+        s = "";
+        for (int i = 0; i < WORD_SIZE; i++) begin
+            if (byte_enable[i]) begin
+                string h;
+                h.hextoa(data[i*8+:8]);
+                if (h.len == 1) begin
+                    h = {"0", h};
+                end
+                s = {h, s};
+            end else begin
+                s = {"__", s};
+            end
+        end
+        byte_mask = s;
+    end endfunction
 
 endmodule
 
@@ -240,55 +327,38 @@ module sim_delay_ram #(
         // first look at our address and reading/writing before nonblocking assignments (NBAs) on the clock edge
         //   change them but display the read data after the NBAs since that isn't available yet
         // note that strobe schedules the probing/displaying of signals to after the NBAs, but returns immediately
-        automatic logic [ADDR_WIDTH-1:BLOCK_ADDR_LSB] v_bc_baddr = s_baddr;  // save the before-clock address
+        logic [ADDR_WIDTH-1:BLOCK_ADDR_LSB] v_bc_baddr;
+        v_bc_baddr = s_baddr;  // save the before-clock address
         if (s_in_final_cycle && s_reading) begin
-            $strobe("%t: [Memory] %s Read finished @ baddr=%x (addr=%x..%x) with data=%x",
-                    $time, PORT_NAME, v_bc_baddr,
-                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}},
+            $strobe("%s[Memory] %s Read finished @ baddr=%x (addr=%x..%x) with data=%x",
+                    DISPLAY_TIME ? $sformatf("%t: ", $time) : "", PORT_NAME, v_bc_baddr,
+                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}},
                     dout); 
         end
         if (s_accepting && s_writing) begin
-            $strobe("%t: [Memory] %s Write accepted @ baddr=%x (addr=%x..%x)",
-                    $time, PORT_NAME, v_bc_baddr,
-                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}});
+            $strobe("%s[Memory] %s Write accepted @ baddr=%x (addr=%x..%x)",
+                    DISPLAY_TIME ? $sformatf("%t: ", $time) : "", PORT_NAME, v_bc_baddr,
+                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}});
         end 
         if (s_in_final_cycle && s_writing) begin
-            $strobe("%t: [Memory] %s Write finished @ baddr=%x (addr=%x..%x)",
-                    $time, PORT_NAME, v_bc_baddr,
-                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}});
+            $strobe("%s[Memory] %s Write finished @ baddr=%x (addr=%x..%x)",
+                    DISPLAY_TIME ? $sformatf("%t: ", $time) : "", PORT_NAME, v_bc_baddr,
+                    {v_bc_baddr, {BLOCK_ADDR_LSB{1'b1}}}, {v_bc_baddr, {BLOCK_ADDR_LSB{1'b0}}});
         end 
         // then use #1 to postpone further processing until nonblocking assignments have completed
-        #1
+        #1;
         // we should now get an early look at what the inputs will be right before the next positive clock edge
         if (s_accepting) begin
             if (s_reading) begin
-                $display("%t: [Memory] %s Read started @ baddr=%x (addr=%x..%x)",
-                         $time, PORT_NAME, s_baddr,
-                         {s_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b1}}});
+                $display("%s[Memory] %s Read started @ baddr=%x (addr=%x..%x)",
+                         DISPLAY_TIME ? $sformatf("%t: ", $time) : "", PORT_NAME, s_baddr,
+                         {s_baddr, {BLOCK_ADDR_LSB{1'b1}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b0}}});
             end 
             if (s_writing) begin
-                $display("%t: [Memory] %s Write started @ baddr=%x (addr=%x..%x) with data=%x",
-                         $time, PORT_NAME, s_baddr,
-                         {s_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b1}}},
+                $display("%s[Memory] %s Write started @ baddr=%x (addr=%x..%x) with data=%x",
+                         DISPLAY_TIME ? $sformatf("%t: ", $time) : "", PORT_NAME, s_baddr,
+                         {s_baddr, {BLOCK_ADDR_LSB{1'b1}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b0}}},
                          s_din);
-            end 
-        end else begin
-            @(negedge clk);
-            #1;
-            // check again right after the negative edge, for another early look at what the inputs will be right
-            //   before the next positive clock edge (the same upcoming positive edge as for the first early look)
-            if (s_accepting) begin
-                if (s_reading) begin
-                    $display("%t: [Memory] %s Read started @ baddr=%x (addr=%x..%x)",
-                             $time, PORT_NAME, s_baddr,
-                             {s_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b1}}});
-                end 
-                if (s_writing) begin
-                    $display("%t: [Memory] %s Write started @ baddr=%x (addr=%x..%x) with data=%x",
-                             $time, PORT_NAME, s_baddr,
-                             {s_baddr, {BLOCK_ADDR_LSB{1'b0}}}, {s_baddr, {BLOCK_ADDR_LSB{1'b1}}},
-                             s_din);
-                end 
             end 
         end
     end
@@ -322,7 +392,7 @@ module sim_xilinx_bram_tdp_nc_nr #(
     always_ff @(posedge clka) begin 
         if (ena) begin
             if (wea) begin
-                r_ram[addra] <= dina;  // warning [VRFC 10-587] is probably safe to ignore
+                r_ram[addra] <= dina;  // warning [VRFC 10-587] is probably safe to ignore, and NBA is important
             end else begin
                 if (!r_ram.exists(addra)) begin  // new random number only on first read
                     rand_data.randomize();
@@ -336,7 +406,7 @@ module sim_xilinx_bram_tdp_nc_nr #(
     always_ff @(posedge clkb) begin 
         if (enb) begin
             if (web) begin
-                r_ram[addrb] <= dinb;  // warning [VRFC 10-587] is probably safe to ignore
+                r_ram[addrb] <= dinb;  // warning [VRFC 10-587] is probably safe to ignore, and NBA is important
             end else begin
                 if (!r_ram.exists(addrb)) begin  // new random number only on first read
                     rand_data.randomize();
--- otter_multicycle_memoryhub_cache_templates/dcache.sv
+++ otter_multicycle_memoryhub_cache_templates/dcache.sv
@@ -2,9 +2,10 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: Keefe Johnson
+//           Joseph Callenes
+//           
 // 
 // Create Date: 02/06/2020 06:40:37 PM
-// Updated Date: 02/13/2020 11:00:00 AM
 // Design Name: 
 // Module Name: dcache
 // Project Name: 
@@ -20,15 +21,274 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+package cache_def;
 
-import memory_bus_sizes::*;
+parameter int TAG_MSB = 31;
+parameter int TAG_LSB = 12;
+
+typedef struct packed{
+    logic valid;
+    logic dirty;
+    logic [TAG_MSB:TAG_LSB] tag;
+}cache_tag_type;
+
+typedef struct {
+    logic [9:0] index;
+    logic we;
+}cache_req_type;
+
+//128-bit cache line
+typedef logic [127:0] cache_data_type;
+
+//CPU request (CPU ->cache controller)
+typedef struct{
+    logic [31:0] addr;
+    logic [31:0] data;
+    logic rw;
+    logic valid;
+}cpu_req_type;
+
+//Cache result (cache controller -> CPU)
+typedef struct {
+    logic [31:0]data;
+    logic ready;
+}cpu_result_type;
+
+//memory request (cache controller -> memory)
+typedef struct {
+    logic [31:0]addr;
+    logic [127:0]data;
+    logic rw;
+    logic valid;
+}mem_req_type;
+
+//memory controller response (memory -> cache controller)
+typedef struct {
+cache_data_type data;
+logic ready;
+}mem_data_type;
+
+endpackage
+
+import cache_def::*;
+import memory_bus_sizes::*; 
+
+module L1_cache_data ( 
+    input clk,
+    input cache_req_type data_req,
+    input cache_data_type data_write,
+    input [3:0] be,
+    input [1:0] block_offset,
+    input from_ram,
+    output cache_data_type data_read);
+    
+    cache_data_type data_mem[0:255];
+    
+    initial begin
+        for(int i=0; i<256; i++)
+            data_mem[i]='0;
+    end
+
+    always_ff @(posedge clk) begin
+        if(data_req.we) begin
+            if(from_ram) 
+                data_mem[data_req.index] <= data_write;
+            if(!from_ram) begin
+              for (int b = 0; b < WORD_SIZE; b++) begin
+                if (be[b]) begin
+                    data_mem[data_req.index][block_offset*WORD_WIDTH+b*8+:8] <= data_write[block_offset*WORD_WIDTH+b*8+:8];  //[b*8+:8];
+                end
+              end
+            end
+        end
+            
+        data_read <= data_mem[data_req.index];
+    end
+endmodule
+
+module L1_cache_tag (
+    input logic clk,
+    input cache_req_type tag_req,
+    input cache_tag_type tag_write,
+    output cache_tag_type tag_read);
+    
+	// tag storage 
+	// incluces valid and dirty bits
+	// async read, sync write
+	
+	cache_tag_type tag_mem[0:255];
+    
+    initial begin
+        for(int i=0; i<256; i++) begin
+            tag_mem[i].valid = 0;
+            tag_mem[i].dirty = 0;
+            tag_mem[i].tag = '0;
+        end
+    end
+    
+    always_comb //read (asynchronous)
+        tag_read = tag_mem[tag_req.index];
+    
+    always_ff @(posedge clk) begin
+        if (tag_req.we)
+            tag_mem[tag_req.index] <= tag_write;
+    end
+	
+endmodule
+
 
 module dcache(
-    input clk,
+    input clk, RESET,
     i_mhub_to_dcache.device mhub,
     i_dcache_to_ram.controller ram
     );
 
+    cpu_req_type cpu_req;     //CPU->cache
+    mem_data_type mem_data;   //memory->cache
+    
+    mem_req_type mem_req;    //cache->memory
+    cpu_result_type cpu_res;  //cache->CPU
+    
+    logic [1:0] block_offset;
+    logic [3:0] be;
+    logic from_ram;
+    logic wait_read, next_wait_read;   
+    
+    assign be = mhub.be;
+    assign block_offset = mhub.waddr[3:2];
+   
+    assign cpu_req.addr[ADDR_WIDTH-1:WORD_ADDR_LSB] = mhub.waddr;
+    assign cpu_req.data = mhub.din;
+    assign cpu_req.rw = mhub.we;
+    assign cpu_req.valid = mhub.en;
+     
+    assign mhub.dout = cpu_res.data;            //async read
+    assign mhub.hold = ~cpu_res.ready; 	 //Inverse     //ready goes high before edge that data is available
+    
+    assign ram.baddr = mem_req.addr[ADDR_WIDTH-1:BLOCK_ADDR_LSB];
+    assign ram.din = mem_req.data;
+    assign ram.we = mem_req.rw;
+    assign ram.en = mem_req.valid;
+   
+    assign mem_data.data = ram.dout;
+    assign mem_data.ready = ~ram.hold;       //Inverse
+    
+    typedef enum {idle, compare_tag, allocate, writeback} cache_state_type;
+   
+    cache_state_type state, next_state;
 
+    cache_tag_type tag_read;
+    cache_tag_type tag_write;
+    cache_req_type tag_req;
+    
+    cache_data_type data_read;
+    cache_data_type data_write;
+    cache_req_type data_req;
+    
+    cpu_result_type next_cpu_res;
+    
+	//FSM for Cache Controller
+    L1_cache_tag L1_tags(.clk(clk), .tag_req(tag_req), .tag_write(tag_write), .tag_read(tag_read));
+    L1_cache_data L1_data(.clk(clk), .data_req(data_req), .data_write(data_write), .be(be),
+                          .block_offset(block_offset), .from_ram(from_ram), .data_read(data_read));
+    
+    logic tag_match;
+   
+    initial begin
+        state = idle;
+        next_state = compare_tag;
+    end
+    
+    always_comb begin
+        tag_match = (tag_read.tag == tag_write.tag);
+        if (state == idle) begin
+            if (cpu_req.valid) begin
+                next_state = compare_tag;
+                cpu_res.ready = 1;
+            end
+            else
+                next_state = idle;
+        end
+        else if (state == compare_tag)
+            if (tag_match && tag_read.valid) //hit
+                next_state = idle;
+            else
+                if (tag_read.dirty)
+                    next_state = writeback;
+                else
+                    next_state = allocate;
+        else if (state == allocate)
+            if (!wait_read)
+                next_state = compare_tag;
+            else
+                next_state = allocate;
+        else if (state == writeback)
+            if (mem_data.ready)
+                next_state = allocate;
+            else
+                next_state = writeback;
+    end
+    
+    always_ff @(posedge clk) begin
+        if (state == idle)
+            if (cpu_req.valid) begin    
+                cpu_res.data <= data_read;
+                tag_req.index <= cpu_res.addr[9:2];
+                tag_req.we <= 1;
+                data_write <= cpu_req.data;
+            end
+    //====================================================// For neatness homie                
+        else if (state == compare_tag) begin
+            if (tag_match && tag_read.valid) begin //hit
+                cpu_res.ready <= 0; //COME BACK PLS
+                data_req.index <= cpu_req.addr[13:6];
+                tag_write.valid <= 1;
+                tag_write.tag <= cpu_res.addr[29:10]; //20 MSB (Due to given code)
+                if (cpu_req.rw) begin //write
+                    tag_write.dirty <= 1;
+                    data_req.we <= 1;
+                end
+                else
+                    tag_write.dirty <= 0;
+            end
+            else begin //miss
+                from_ram <= 1;
+                if (tag_read.dirty) begin
+                    next_state = writeback; //writeback policy step
+                    mem_req.addr <= mhub.waddr; //Check this later (if signal direction is correct)
+                    mem_req.en <= 1;
+                    mem_req.we <= mhub.we;
+                    mem_req.din <= mhub.din;
+                end
+                else
+                    next_state = allocate;
+                    next_wait_read <= 1;
+            end
+        end
+        //====================================================// For neatness homie 
+        else if (state == allocate) begin
+                if (!wait_read)
+                    next_state = compare_tag;
+                else begin
+                    if (mem_req.we) begin
+                        data_write <= mem_data.data;
+                        next_wait_read <= 0;
+                    end
+                    next_state = allocate;
+                end
+        end
+        //====================================================// For neatness homie 
+        else if (state == writeback) begin
+            if (mem_data.ready) begin
+                next_wait_read <= 1;
+                next_state = allocate;
+            end
+            else
+                next_state = writeback;
+        end
+        
+        state <= next_state;
+        wait_read <= next_wait_read;
+    end
 
 endmodule
